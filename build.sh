f655eb035a9d0411938260910186c62126aa184d
Matthew Chong <100985071@student.swin.edu.au>
2022-01-02 12:34:33 +1100

    some basic changes for portability

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+if [[ $* ]]; then
+  docker build . -t ghcr.io/thinkier/tcp-h264-server-armv7-build
+fi
+
 # Cross compile
 docker run -v "$(pwd):/app" ghcr.io/thinkier/tcp-h264-server-armv7-build
