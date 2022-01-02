e8333b8ca7042cb4aed4a18150cc698d1b7a8160
thinkier <26728734+thinkier@users.noreply.github.com>
2022-01-02 17:36:02 +1100

    stitched constituent functions together

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 #[macro_use]
 extern crate argh;
 
-use std::net::{Ipv4Addr, SocketAddrV4};
-use std::process;
+use std::collections::{HashMap, HashSet};
+use std::net::SocketAddr;
 use std::sync::Arc;
 
-use tokio::io;
-use tokio::io::stdin;
+use tokio::io::{AsyncWriteExt, stdin};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::mpsc::Sender;
-use tokio::sync::Mutex;
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::{mpsc, Mutex};
 
 use crate::cli::CliArgs;
 use crate::h264::{H264NalUnit, H264Stream};
@@ -18,6 +17,7 @@ mod cli;
 mod h264;
 
 pub type Am<T> = Arc<Mutex<T>>;
+pub type SocksContainer = Am<HashMap<SocketAddr, TcpStream>>;
 
 pub fn am<T>(t: T) -> Am<T> {
 	Arc::new(Mutex::new(t))
@@ -26,8 +26,13 @@ pub fn am<T>(t: T) -> Am<T> {
 #[tokio::main]
 async fn main() {
 	let args = argh::from_env::<CliArgs>();
+	let (tx,rx) = mpsc::channel(60);
+	let socks = am(HashMap::new());
+	tokio::spawn(read_h264_stream(tx));
+	tokio::spawn(write_h264_stream(rx, socks.clone()));
 
 	let listener = args.start_listening().await;
+	listen_for_new_sockets(listener, socks).await;
 }
 
 async fn read_h264_stream(consumer: Sender<H264NalUnit>) {
@@ -38,12 +43,67 @@ async fn read_h264_stream(consumer: Sender<H264NalUnit>) {
 	}
 }
 
-async fn listen_for_new_sockets(listener: TcpListener, socks: Am<Vec<Am<TcpStream>>>) {
-	while let Ok((client, addr)) = listener.accept().await {
-		let client = am(client);
-		// Register socket with the reader thread
-		{
-			socks.lock().await.push(client.clone());
+async fn write_h264_stream(mut producer: Receiver<H264NalUnit>, socks: SocksContainer) {
+	let mut parameters = Vec::with_capacity(2);
+	let mut frame_buffer = Vec::with_capacity(100);
+	let mut known_socks = HashSet::new();
+
+	while let Some(frame) = producer.recv().await {
+		match frame.unit_code {
+			7 | 8 => {
+				parameters.push(frame);
+				continue;
+			}
+			5 => frame_buffer.clear(),
+			_ => {}
 		}
+
+		let mut errors = Vec::with_capacity(0);
+
+		{
+			for (addr, sock) in socks.lock().await.iter_mut() {
+				let write = if !known_socks.contains(addr) {
+					known_socks.insert(addr.to_owned());
+
+					let mut buffer = Vec::with_capacity(
+						parameters.iter().fold(0, count_nal_bytes) +
+							frame_buffer.iter().fold(0, count_nal_bytes) +
+							frame.raw_bytes.len()
+					);
+
+					for nal in &parameters {
+						buffer.extend(&nal.raw_bytes);
+					}
+					for nal in &frame_buffer {
+						buffer.extend(&nal.raw_bytes);
+					}
+					buffer.extend(&frame.raw_bytes);
+
+					sock.write_all(&buffer).await
+				} else {
+					sock.write_all(&frame.raw_bytes).await
+				};
+
+				if let Err(_) = write {
+					errors.push(addr.to_owned());
+				}
+			}
+		}
+
+		while let Some(addr) = errors.pop() {
+			socks.lock().await.remove(&addr);
+		}
+
+		frame_buffer.push(frame);
+	}
+}
+
+fn count_nal_bytes(x: usize, nal: &H264NalUnit) -> usize {
+	x + nal.raw_bytes.len()
+}
+
+async fn listen_for_new_sockets(listener: TcpListener, socks: SocksContainer) {
+	while let Ok((client, addr)) = listener.accept().await {
+		socks.lock().await.insert(addr, client);
 	}
 }
